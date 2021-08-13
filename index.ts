44b5f5e5ebf0311d4e22c16951fe998bd31328cc
Tom Oliveira Bastos <washington-kd@hotmail.com>
2021-08-12 23:44:49 -0300

    commit via script Thu Aug 12 23:44:48 -03 2021

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ const personagem = new Personagem({
        x: 0,
        y: 0,
        size: 20,
-       speed : 0.01
+       speed : 0.1
 });
 
 const canvas : HTMLCanvasElement = document.createElement("canvas")
@@ -17,13 +17,31 @@ canvas.width = 400
 document.body.appendChild(canvas)
 
 const engine = new Engine(ctx)
-engine.update((p: Personagem) => {
+engine.update((p: Personagem, canvas : HTMLCanvasElement) => {
+
+       if(p.getY() > canvas.height){
+
+              let speed = p.getSpeed()
+
+              speed *= -0.99
+
+              p.setSpeed(speed);
+
+       }
+
+       p.setSpeed(p.getSpeed() + 0.1)
+
        const newPosY = p.getY() + p.getSpeed()
+
        p.setY(newPosY)
-}, personagem)
+
+}, personagem, canvas)
 engine.render((p: Personagem, ctx : CanvasRenderingContext2D, canvas : HTMLCanvasElement) => {
+
        ctx.clearRect(0, 0, canvas.width, canvas.height)
+
        ctx.fillRect(p.getX(), p.getY(), p.getSize(), p.getSize())
+
 }, personagem, ctx, canvas)
 
 engine.run()
