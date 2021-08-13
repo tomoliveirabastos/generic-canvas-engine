1f1b4ebcee506210a6b41419ca2f1b49e42f56d4
Tom Oliveira Bastos <washington-kd@hotmail.com>
2021-08-13 00:49:55 -0300

    commit via script Fri Aug 13 00:49:55 -03 2021

---
+++ src/Render/Render.ts
@@ -0,0 +1,13 @@
+import { Personagem } from "../Personagem";
+
+interface IRenderMethods{
+       main(p: Personagem, ctx: CanvasRenderingContext2D, canvas: HTMLCanvasElement) : void
+}
+class Render implements IRenderMethods{
+       public main (p: Personagem, ctx: CanvasRenderingContext2D, canvas: HTMLCanvasElement){
+              ctx.clearRect(0, 0, canvas.width, canvas.height);
+              ctx.fillRect(p.getX(), p.getY(), p.getSize(), p.getSize());
+       }
+}
+
+export {Render}
